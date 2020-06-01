ceeeb0169f708ce3fbf290a304b932d4767712b3
OfirFlorentz <ofir.florentz@ibm.com>
2020-06-01 23:26:17 +0300

    added time limit added bfs hurstic

--- MinimaxPlayer.py
+++ MinimaxPlayer.py
@@ -1,4 +1,5 @@
 from time import time as _time
+import copy
 
 
 class MinimaxPlayer():
@@ -34,14 +35,22 @@ class MinimaxPlayer():
             for j, val in enumerate(row):
                 if val == 1:
                     counter += 1
+                if counter > 1:
+                    print(5)
         return counter
 
     def make_move(self, time):  # time parameter is not used, we assume we have enough time.
+        depth = 1
         ID_start_time = _time()
-        depth = 0
-        while (depth < 18):
+        while True:
+            print(depth)
+            copy_self = copy.deepcopy(self)
             assert self.count_ones(self.board) == 1
-            best_move, best_move_score, best_new_loc = self.rb_minmax(depth)
+            try:
+                best_move, best_move_score, best_new_loc = copy_self.rb_minmax(depth, time - _time() + ID_start_time - 0.05,
+                                                                          copy.deepcopy(self.board))
+            except TimeoutError:
+                break
             assert self.count_ones(self.board) == 1
             depth += 1
 
@@ -56,94 +65,96 @@ class MinimaxPlayer():
         # print('returning move', best_move)
         return best_move
 
-
-    def rb_minmax(self, depth, my_turn=True):
-        assert self.count_ones(self.board) == 1
+    def rb_minmax(self, depth, time_left, board, my_turn=True):
+        start = _time()
+        assert self.count_ones(board) == 1
         if depth == 0:
-            if my_turn == 1: # our turn
-                return None, self.state_score(self.loc, my_turn), None
-            else:
-                return None, self.state_score(self.rival_position, my_turn), None
+            return None, self.state_score(my_turn, board), None
 
         if my_turn == 1:
             best_move, max_score, best_new_loc = None, float('-inf'), None
             prev_loc = self.loc
-            self.board[prev_loc] = -1
+            board[prev_loc] = -1
             for d in self.directions:
+                if _time() - start > time_left:
+                    raise TimeoutError()
                 i = prev_loc[0] + d[0]
                 j = prev_loc[1] + d[1]
-                if 0 <= i < len(self.board) and 0 <= j < len(self.board[0]) and self.board[i][j] == 0:  # then move is legal
+                if 0 <= i < len(board) and 0 <= j < len(board[0]) and board[i][j] == 0:  # then move is legal
                     new_loc = (i, j)
                     # print('prev loc', prev_loc, 'new_loc:', new_loc, 'move:', (i, j))
-                    assert self.board[new_loc] == 0
-                    self.board[new_loc] = 1
+                    assert board[new_loc] == 0
+                    board[new_loc] = 1
                     self.loc = new_loc
-                    _, score, _ = self.rb_minmax(depth-1, 1-my_turn)
+                    _, score, _ = self.rb_minmax(depth-1,  time_left -_time() + start, board, 1-my_turn)
                     # assert -1<= score <= 1
-                    self.board[new_loc] = 0
+                    board[new_loc] = 0
                     if score > max_score or max_score == float('-inf'):
                         best_move, max_score, best_new_loc = d, score, new_loc
                         # print(best_move, max_score, best_new_loc)
-                    assert self.count_ones(self.board) == 0
+                    assert self.count_ones(board) == 0
             self.loc = prev_loc
-            self.board[self.loc] = 1
+            board[self.loc] = 1
             return best_move, max_score, best_new_loc
 
 
         else:
             best_move, min_score, best_new_loc = None, float('inf'), None
             prev_loc = self.rival_position
-            self.board[prev_loc] = -1
+            board[prev_loc] = -1
             for d in self.directions:
+                if _time() - start > time_left:
+                    raise TimeoutError()
                 i = prev_loc[0] + d[0]
                 j = prev_loc[1] + d[1]
-                if 0 <= i < len(self.board) and 0 <= j < len(self.board[0]) and self.board[i][j] == 0:  # then move is legal
+                if 0 <= i < len(board) and 0 <= j < len(board[0]) and board[i][j] == 0:  # then move is legal
                     new_loc = (i, j)
                     # print('prev loc', prev_loc, 'new_loc:', new_loc, 'move:', (i, j))
-                    assert self.board[new_loc] == 0
+                    assert board[new_loc] == 0
                     self.rival_position = new_loc
-                    self.board[new_loc] = 2
-                    _,score,_ = self.rb_minmax(depth-1, 1-my_turn)
-                    print(score)
-                    self.board[new_loc] = 0
-                    if score < min_score  or min_score == float('inf'):
+                    board[new_loc] = 2
+                    _, score, _ = self.rb_minmax(depth-1, time_left -_time() + start, board, 1-my_turn)
+                    # print(score)
+                    board[new_loc] = 0
+                    if score < min_score or min_score == float('inf'):
                         best_move, min_score, best_new_loc = d, score, new_loc
             self.rival_position = prev_loc
-            self.board[self.rival_position] = 2
+            board[self.rival_position] = 2
             return best_move, min_score, best_new_loc
 
-    def state_score(self, loc, turn):
+    def state_score(self, my_turn, board):
         num_steps_available = 0
+        num_steps_available_opp = 0
         for d in self.directions:
-            i = loc[0] + d[0]
-            j = loc[1] + d[1]
-            if 0 <= i < len(self.board) and 0 <= j < len(self.board[0]) and self.board[i][j] == 0:  # then move is legal
+            i = self.loc[0] + d[0]
+            j = self.loc[1] + d[1]
+            if 0 <= i < len(board) and 0 <= j < len(board[0]) and board[i][j] == 0:  # then move is legal
                 num_steps_available += 1
 
-        # lossing, final position
-        if num_steps_available == 0:
-            if turn == 1:
-                return -1
+            i = self.rival_position[0] + d[0]
+            j = self.rival_position[1] + d[1]
+            if 0 <= i < len(board) and 0 <= j < len(board[0]) and board[i][j] == 0:  # then move is legal
+                num_steps_available_opp += 1
+
+        # end of game
+        if num_steps_available == num_steps_available_opp == 0:
+            return 0
+        if my_turn and num_steps_available == 0:
+            return -1
+        if not my_turn and num_steps_available_opp == 0:
             return 1
 
-        num_steps_available = 0
+        distance_from_start = abs(self.starting_position[0] - self.loc[0]) + abs(self.starting_position[1] - self.loc[1])
+        distance_from_start_opp = abs(self.rival_starting_position[0] - self.rival_position[0])\
+                                  + abs(self.rival_starting_position[1] - self.rival_position[1])
+
+        return (num_steps_available - num_steps_available_opp + distance_from_start - distance_from_start_opp) / (4 + 2*board.size)
+
+    def bfs(self, loc, board, counter=0):
         for d in self.directions:
             i = loc[0] + d[0]
             j = loc[1] + d[1]
-            if 0 <= i < len(self.board) and 0 <= j < len(self.board[0]) and self.board[i][j] == 0:  # then move is legal
-                num_steps_available += 1
-
-
-        if turn == 1:  # our turn
-            distance_from_start = abs(self.starting_position[0] - loc[0]) + abs(self.starting_position[1] - loc[1])
-            distance_from_rival = -1 * abs(self.rival_position[0] - loc[0]) + abs(self.rival_position[1] - loc[1])
-            # state score is bounded between -1 to 1
-            return (num_steps_available + distance_from_rival + distance_from_start) / (4 + 2*self.board.size)
-
-        else: # rival turn
-            distance_from_start = abs(self.rival_starting_position[0] - loc[0])\
-                                  + abs(self.rival_starting_position[1] - loc[1])
-            distance_from_rival = -1 * abs(self.loc[0] - loc[0]) + abs(self.loc[1] - loc[1])
-            # state score is bounded between -1 to 1
-            return -1*(num_steps_available + distance_from_rival + distance_from_start) / (4 + 2 * self.board.size)
-
+            if 0 <= i < len(board) and 0 <= j < len(board[0]) and board[i][j] == 0:  # then move is legal
+                board[i][j] = -1
+                counter += self.bfs((i,j), board)
+        return counter
