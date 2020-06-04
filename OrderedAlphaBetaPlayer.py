6262d24013f05bf5624ed1e865ea04214da16a3b
nadavru <63553528+nadavru@users.noreply.github.com>
2020-06-05 01:05:11 +0300

    Update OrderedAlphaBetaPlayer.py

--- OrderedAlphaBetaPlayer.py
+++ OrderedAlphaBetaPlayer.py
@@ -1,49 +1,13 @@
 from time import time as _time
 import copy
 from hashlib import sha256
+from AlphaBetaPlayer import AlphaBetaPlayer
 
 
-class OrderedAlphaBetaPlayer():
+class OrderedAlphaBetaPlayer(AlphaBetaPlayer):
     def __init__(self):
-        self.loc = None
-        self.board = None
-        self.directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-        self.starting_position = None
-        self.rival_starting_position = None
-        self.rival_position = None
-        self.available = 0
+        super().__init__()
         self.score_dict = {}
-
-    def set_game_params(self,board):
-        self.board = board
-        for i, row in enumerate(board):
-            for j, val in enumerate(row):
-                if val == 1:
-                    self.loc = (i, j)
-                    self.starting_position = (i,j)
-                if val == 2:
-                    self.rival_position = (i,j)
-                    self.rival_starting_position = (i,j)
-                if val == 0:
-                    self.available += 1
-
-    def set_rival_move(self, loc):
-        self.board[loc] = 2
-        self.board[self.rival_position] = -1
-        self.rival_position = loc
-
-    def count_players(self, board):
-        counter1 = 0
-        counter2 = 0
-        for i, row in enumerate(board):
-            for j, val in enumerate(row):
-                if val == 1:
-                    counter1 += 1
-                if val ==2:
-                    counter2 +=1
-        if counter1 !=1 or counter2 != 1:
-            print(5)
-        return (counter1, counter2)
 
     def make_move(self, time):  # time parameter is not used, we assume we have enough time.
         depth = 1
@@ -113,6 +77,7 @@ class OrderedAlphaBetaPlayer():
                         
             self.loc = prev_loc
             board[self.loc] = 1
+            self.score_dict[hash256(path)]=max_score
             return best_move, max_score, best_new_loc, self.score_dict
 
 
@@ -146,67 +111,5 @@ class OrderedAlphaBetaPlayer():
                         break
             self.rival_position = prev_loc
             board[self.rival_position] = 2
+            self.score_dict[hash256(path)]=min_score
             return best_move, min_score, best_new_loc, self.score_dict
-
-    def state_score(self, my_turn, board):
-        num_steps_available = 0
-        num_steps_available_opp = 0
-        for d in self.directions:
-            i = self.loc[0] + d[0]
-            j = self.loc[1] + d[1]
-            if 0 <= i < len(board) and 0 <= j < len(board[0]) and board[i][j] == 0:  # then move is legal
-                num_steps_available += 1
-
-            i = self.rival_position[0] + d[0]
-            j = self.rival_position[1] + d[1]
-            if 0 <= i < len(board) and 0 <= j < len(board[0]) and board[i][j] == 0:  # then move is legal
-                num_steps_available_opp += 1
-
-        # end of game
-        if num_steps_available == num_steps_available_opp == 0:
-            return 0
-        if my_turn and num_steps_available == 0:
-            return -1
-        if not my_turn and num_steps_available_opp == 0:
-            return 1
-
-        # get params
-        distance_from_start = abs(self.starting_position[0] - self.loc[0]) + abs(self.starting_position[1] - self.loc[1])
-        distance_from_start_opp = abs(self.rival_starting_position[0] - self.rival_position[0])\
-                                  + abs(self.rival_starting_position[1] - self.rival_position[1])
-        available_steps, found_opp = self.dfs(self.loc, copy.deepcopy(board))
-        available_steps_opp, _ = self.dfs(self.rival_position, copy.deepcopy(board))
-
-        # cant block each other, the one who have more steps win
-        if not found_opp:
-            if available_steps > available_steps_opp + int(my_turn):
-                return 0.999
-            elif available_steps + int(not my_turn) < available_steps_opp :
-                return -0.999
-            return 0
-
-
-        # norm
-        distance_from_start = distance_from_start / (2*len(board))
-        distance_from_start_opp = distance_from_start_opp / (2*len(board))
-
-        available_steps = available_steps / (self.available + 0.001)
-        available_steps_opp = available_steps_opp / (self.available + 0.001)
-
-
-
-        return (available_steps - available_steps_opp + distance_from_start - distance_from_start_opp) / 2
-
-    def dfs(self, loc, board, max_length=0, found_opp=False):
-        for d in self.directions:
-            i = loc[0] + d[0]
-            j = loc[1] + d[1]
-            if 0 <= i < len(board) and 0 <= j < len(board[0]):
-                if board[i][j] == 0:  # then move is legal
-                    board[i][j] = -1
-                    length, found = self.dfs((i,j), board, found_opp)
-                    max_length = max(max_length,1 + length)
-                    found_opp = found_opp or found
-                elif board[i][j] == 2:
-                    found_opp = True
-        return max_length, found_opp
